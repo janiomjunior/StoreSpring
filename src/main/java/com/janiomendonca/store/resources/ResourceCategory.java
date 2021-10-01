769aebccbe2f15ef7c4e595df09f2f7dc8493a95
Janio Mendonca <janiomjunior@hotmail.com>
2021-10-01 15:30:45 -0400

    Testing the first domain class-category

--- src/main/java/com/janiomendonca/store/resources/ResourceCategory.java
+++ src/main/java/com/janiomendonca/store/resources/ResourceCategory.java
@@ -1,17 +1,30 @@
 package com.janiomendonca.store.resources;
 
-import org.springframework.web.bind.annotation.RestController;
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.janiomendonca.store.domain.Category;
 
 @RestController
 @RequestMapping(value="/Categories")
 public class ResourceCategory {
 	
 	@RequestMapping(method=RequestMethod.GET)
-	public String listing () {
+	public List<Category> listing () {
 		
-		return "REST is working";
+		Category cat1 = new Category (1, "Informatics");
+		Category cat2 = new Category (2, "Officer");
+		
+		List<Category> list = new ArrayList<>();
+		list.add(cat1);
+		list.add(cat2);
+		
+		
+		return list;
 		
 	}
 }
